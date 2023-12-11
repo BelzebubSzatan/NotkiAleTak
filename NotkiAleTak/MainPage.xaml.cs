4467307d42d1c04e486196a7ac5c9eae6c1b8004
KacperMatlag <matlagkacper@gmail.com>
2023-12-11 19:05:44 +0100

    Basic UiAdd project files.

---
+++ NotkiAleTak/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using NotkiAleTak.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NotkiAleTak {
+    public partial class MainPage : ContentPage {
+        List<NoteModel> note=new List<NoteModel>();
+        public MainPage() {
+            InitializeComponent();
+            note.Add(new NoteModel() { 
+                ID=Guid.NewGuid(),
+                Title="Title",
+            });
+            note.Add(new NoteModel() {
+                ID = Guid.NewGuid(),
+                Title = "Title2",
+            });
+            NoteList.ItemsSource = note;
+        }
+
+        private void Add_Clicked(object sender, EventArgs e) {
+
+        }
+
+        private void Edit_Clicked(object sender, EventArgs e) {
+
+        }
+
+        private void Delete_Clicked(object sender, EventArgs e) {
+
+        }
+    }
+}
---
+++ NotkiAleTak/Model/NoteModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotkiAleTak.Model {
+    public class NoteModel {
+        public Guid ID { get; set; }
+        public string Title { get; set; }
+    }
+}
